04e4086b20476602484b8f83353ec910ef39a954
etechshweta <shweta_sharma@excellencetechnologies.in>
2018-07-04 12:08:24 +0530

    login

--- login/custum.js
+++ login/custum.js
@@ -1,34 +1,32 @@
   $(document).ready(function(){
     $("#submit").click(function(e){
-
         e.preventDefault();
         var username = $("#txtUsername").val();
         var password = $("#txtPassword").val();
         if (username != '' && password!=''){
-        $.ajax
-        ({
-        type: "GET",
-        url: "data.json",
-        dataType: "json",
-        success: function (result) {
-            $.each(result, function(i, data ) {
-                                if (username != '' && password!='')
-
-                                    {
-                                        if(username==data.username && password==data.password){
-                                          $("#msg").html("login sucessful");
-                                          return false;
-                                        }
-                                        else{
-                                          $("#msg").html("login unsucessful");
-                                        }
-                                    }
-                                     });
-                                    }
-        });
-      }
-      else {
-        $("#msg").html("Please fill all the fields");
-      }
-        });
-            });
+          $.ajax
+          ({
+            type: "GET",
+            url: "data.json",
+            dataType: "json",
+            success: function (result) {
+              $.each(result, function(i, data ) {
+                  if (username != '' && password!='')
+                      {
+                      if(username==data.username && password==data.password){
+                        $("#msg").html("login sucessful");
+                        return false;
+                      }
+                      else{
+                        $("#msg").html("login unsucessful");
+                      }
+                      }
+              });
+            }
+          });
+        }
+        else {
+          $("#msg").html("Please fill all the fields");
+        }
+    });
+  });
